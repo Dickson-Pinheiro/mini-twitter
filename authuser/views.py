1a9fa0aae89eb57efd1052224b6afbfbfb1de574
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-05-23 19:37:31 -0300

    feat: create user

--- authuser/views.py
+++ authuser/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        User.objects.create_user(email=request.data['email'],
+                                  password=request.data['password'],
+                                  username=request.data['username']
+                                  )
+        return Response(status=status.HTTP_201_CREATED)
