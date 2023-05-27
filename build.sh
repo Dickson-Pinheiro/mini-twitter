175fb0df53908f5ae6da34b2a9c36d3fd1252e20
Dickson Pinheiro <dicksonpinheiro4@gmail.com>
2023-05-27 20:58:55 -0300

    update bash build

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install -U pip
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+python3 manage.py collectstatic --no-input
+python3 manage.py migrate
